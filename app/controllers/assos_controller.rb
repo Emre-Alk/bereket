0d4226df4934c9b73c8d25a4b1b904d0946560c7
Emre ALKAN <alkan.emre@hotmail.fr>
2024-06-27 18:33:27 +0200

    all dynamic except chart

--- app/controllers/assos_controller.rb
+++ app/controllers/assos_controller.rb
@@ -6,11 +6,24 @@ class AssosController < ApplicationController
     @my_asso = current_user.asso
     @my_place = @my_asso.places.first # "first" afin de faire simple pour l'exemple mais reprendre pour carousselle
     @donations = @my_place.donations
-    @table = @donations.map { |i| [i.donator.id, i.amount] }
+    table = @donations.map { |i| [i.donator, i.amount] }
+    # somme des dons
     @sum = 0
-    @table.each do |e|
+    table.each do |e|
       @sum += e[1]
     end
+    # top donateurs
+    hash = {}
+    table.each do |e|
+      if hash.key?(e[0])
+        hash[e[0]] += e[1]
+      else
+        hash[e[0]] = e[1]
+      end
+    end
+    @top = hash.sort_by { |key, value| -value }.to_h
+    # last donations (plus recent au plus ancien)
+    @sorted_donations = @donations.order(occured_on: :desc)
   end
 
   def new
--- app/models/donator.rb
+++ app/models/donator.rb
@@ -2,6 +2,7 @@ class Donator < ApplicationRecord
   belongs_to :user
   has_many :donations
   has_many :places, through: :donations
+  has_one_attached :profile_image
 
   validates :first_name, :last_name, :email, presence: true
 end
