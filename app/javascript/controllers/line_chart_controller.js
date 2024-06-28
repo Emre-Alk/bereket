70cb5d5d08f0b2d4fe130d44dc72564badadfc9a
Emre ALKAN <alkan.emre@hotmail.fr>
2024-06-28 16:36:50 +0200

    importmap pin chart.js - debbug importmap.rb avec from url

---
+++ app/javascript/controllers/line_chart_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+import { Chart, registerables } from "chart.js"
+Chart.register(...registerables)
+
+
+// Connects to data-controller="line-chart"
+export default class extends Controller {
+  connect() {
+    console.log("hello chart");
+  }
+}
