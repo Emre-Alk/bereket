299bb939755611e8e4fcc9c28fecab8dc6f457a5
Emre ALKAN <alkan.emre@hotmail.fr>
2024-07-02 21:53:33 +0200

    chart done - dashboard data debugged - dashboard view done - remains: menu button

--- app/controllers/assos_controller.rb
+++ app/controllers/assos_controller.rb
@@ -31,22 +31,23 @@ class AssosController < ApplicationController
 
     # ===== revenue of the current month ===== start
     start_month = Date.today.beginning_of_month
-    end_month = Date.today.end_of_month
+    # astuce car 'Date.today' seul ne comptera pas les dons du cours de la journée car occured_on etc.. de class Time
+    end_month = Date.today.next - 1.seconds
     # current_month sent to the view so can fetch it to the chart.js controller with stymulus for setting the time axis
-    current_month = (start_month..end_month)
+    @current_month = (start_month..end_month)
     # here I filter all donations occured this month
-    @donations_current_month = @donations.where(occured_on: current_month).order(occured_on: :desc)
-    # creer un hash dont 'key' sera 1 jour du mois (ex: Thu, 27, Jun 2024), et la 'value' sera un array des dons qui auront la meme "occured_on"
+    @donations_current_month = @donations.where(occured_on: start_month..end_month).order(occured_on: :asc)
+    # creer un hash dont 'key' sera 1 jour du mois (ex: Thu, 27, Jun 2024), et la 'value' sera un array d'instances de dons qui auront la meme "occured_on"
     @donations_per_day_current_month = @donations_current_month.group_by { |don| don.occured_on.to_date }
     # creer un array avec jour du mois (key) en numéro de jour (ex 27) et la 'value' egale à la somme des 'ammout' des dons dans l'array de dons précédent
     @revenue_per_day_current_month = @donations_per_day_current_month.map { |date, dons| [date.strftime('%d'), dons.sum(&:amount)] }
     # The &: syntax is shorthand for converting a symbol to a proc (a block of code). It is equivalent to "dons.sum { |don| don.amount }"
     # if is necessary for the case where no donation occured on a day. This would led to the absence of this day in the above revenue array.
-    if @revenue_per_day_current_month.length < current_month.to_a.length
+    if @revenue_per_day_current_month.length < @current_month.to_a.length
       # pour pouvoir utiliser la methode 'fetch' plus bas
       revenue_hash = @revenue_per_day_current_month.to_h
       # créer un array de jour du mois complet pour pouvoir le comparer avec @revenue...month et deceler des jours manquants
-      @array_complete_days_current_month = current_month.to_a.map { |day| day.strftime('%d') }
+      @array_complete_days_current_month = @current_month.to_a.map { |day| day.strftime('%d') }
       # pour chaque jour manquant dans revenue, je remplace la indice[1] par 0 => ex [[26,x€], [27, 0]...[30,x€]]
       revenue_complete_days_current_month = @array_complete_days_current_month.map { |day| [day, revenue_hash.fetch(day, 0)] }
       @revenue_per_day_current_month = revenue_complete_days_current_month
--- app/javascript/controllers/line_chart_controller.js
+++ app/javascript/controllers/line_chart_controller.js
@@ -9,10 +9,55 @@ export default class extends Controller {
     revenue: Array,
     timeframe: Array
   }
+  static targets = ["revenueChart"]
 
   connect() {
+    const data = this.revenueValue.map((earning) => earning[1]/100.0)
+    const labels = this.revenueValue.map((day) => day[0])
 
-  // creer le graph ici
-
+    const ctx = this.revenueChartTarget
+    const config = {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          labels: 'Revenu en €',
+          data: data,
+          borderColor: 'rgba(0, 0, 0, 1)',
+          pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+          pointBorderColor: 'rgba(0, 0, 0, 1)',
+          borderCapStyle: 'rounded',
+          borderWidth: 3,
+          pointHitRadius: 1,
+          pointStyle: 'circle',
+          fill: false,
+        }]
+      },
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: 'Ce mois-ci'
+          },
+          legend: {
+            display: false
+          }
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false
+            }
+          },
+          y: {
+            beginAtZero: true,
+            grid: {
+              display: false,
+            }
+          }
+        }
+      }
+    }
+    new Chart(ctx, config)
   }
 }
