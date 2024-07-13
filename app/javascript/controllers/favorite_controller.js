57c938ba5629b3509928cf5e12688e2604ab4205
Emre ALKAN <alkan.emre@hotmail.fr>
2024-07-13 12:27:23 +0200

    modif JS fav actuate with toggle classes

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -11,10 +11,15 @@ export default class extends Controller {
   actuate(event) {
     event.preventDefault()
     const heart = this.heartTarget.querySelector("#heart")
+    console.log(heart);
     if (heart.getAttribute("style")) {
       heart.removeAttribute("style")
+      heart.classList.toggle("fa-regular")
+      heart.classList.toggle("fa-solid")
     } else {
       heart.setAttribute("style", "color: red")
+      heart.classList.toggle("fa-regular")
+      heart.classList.toggle("fa-solid")
       heart.classList.add("fa-2xl")
       heart.classList.add("fa-bounce")
       setTimeout(() => {
