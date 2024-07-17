f67ccbe7f097ae6f0831f042f0e7443163ce6989
Emre ALKAN <alkan.emre@hotmail.fr>
2024-07-17 02:26:53 +0200

    JS donation ctrl done with toggling confirmation box and value inside form etc.. remains: confirm action with go to stripe and submit form to server

--- app/javascript/controllers/donation_controller.js
+++ app/javascript/controllers/donation_controller.js
@@ -2,28 +2,45 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="donation"
 export default class extends Controller {
-  static targets = [ 'inputfield', 'confirmBox' ]
+  static targets = [ 'inputfield', 'confirmBox', 'monDon']
+
   connect() {
     console.log('hello donation');
-    this.toggle = this.toggle.bind(this)
+    const confirmBox = this.confirmBoxTarget
+    this.donationForm = document.getElementById("formDon")
   }
 
-  selected(event) {
-    const button = event.target
-  // pass the button black and set the value of the inputfield of the form with its own value
-    button.classList.toggle("bg-white")
-    button.classList.toggle("bg-black")
-    button.classList.toggle("text-white")
-    this.inputfieldTarget.value = button.value
-  // popup confirmation page
-    this.confirmBoxTarget.classList.toggle("-translate-y-full")
-    let boxOpen = this.confirmBoxTarget.classList.contains("-translate-y-full")
+  showConfirm(event) {
+    event.preventDefault()
+    confirmBox.classList.toggle("-translate-y-full")
+    let boxOpen = confirmBox.classList.contains("-translate-y-full")
     if (boxOpen) {
-      document.addEventListener('touchstart', this.toggle)
+      this.inputfieldTarget.value = event.target.value
+    } else {
+      document.getElementById("formDon").reset()
     }
+    event.stopPropagation()
   }
 
-  toggle(event) {
-    const touched = this.confirmBoxTarget.contains(event.target)
+  hideConfirmation(event) {
+    console.log(event.target);
+    if (confirmBox.contains(event.target)) return
+      confirmBox.classList.remove("-translate-y-full")
+      document.getElementById("formDon").reset()
+
+  }
+
+  confirmTip() {
+    // Implement your confirmation logic here
+    console.log('go stripe')
+    // this.hideConfirmation()
+    confirmBox.classList.remove("-translate-y-full")
+
+  }
+
+  cancelTip() {
+    // this.hideConfirmation()
+    confirmBox.classList.remove("-translate-y-full")
+
   }
 }
