1f402bcca371092d0e328e49973a988ec7cfd9c2
Emre ALKAN <alkan.emre@hotmail.fr>
2024-07-18 19:50:58 +0200

    2# JS donation ctrl done except: checkout and fetch

--- app/javascript/controllers/donation_controller.js
+++ app/javascript/controllers/donation_controller.js
@@ -10,12 +10,15 @@ export default class extends Controller {
   connect() {
     console.log('hello donation');
     const confirmBox = this.confirmBoxTarget
-    // this.donationForm = document.getElementById("formDon")
     this.overlay = document.getElementById('overlay')
-    this.customValue = this.customValue.bind(this)
+    this.submitInput = this.submitInput.bind(this) // this set 'this' in submitinput action equal to 'this' of the controller
   }
 
   showConfirm(event) {
+    // action used to toggle the confirmation box either if:
+    // user hit a quick button (event exist), or
+    // user type a custom value into the input field (event don't exist => 'else' scenario)
+
     if (event) {
       console.log('w/ event');
       event.preventDefault()
@@ -38,26 +41,41 @@ export default class extends Controller {
   }
 
   hideConfirmation(event) {
-    if (confirmBox.contains(event.target) || event.target === this.submitBtnTarget ) return
+    // this is an endpoint of a user flow so many hidden and disabled stuff must be recalled here
+    // in order to get back to the starting point (complete circle)
+    // check if event target (user touch) happened on the confirmation box or was from actuation of submit btn or from focus on input field
+    // if either one is true, then exists but do nothing (thanks to return)
+
+    if (confirmBox.contains(event.target) || event.target === this.submitBtnTarget || event.target === this.inputfieldTarget) return
+    // if one true (user hit on empty spaces on page), then execute these lines
       console.log('hide');
       confirmBox.classList.remove("-translate-y-full")
-      // document.getElementById("formDon").reset()
       this.inputfieldTarget.value = ''
+      this.submitBtnTarget.setAttribute('disabled', 'true')
       this.overlay.classList.add('hidden')
+      this.messageTarget.classList.add('hidden')
   }
 
   showSubmit() {
-
+    // this is specific to the scenario, user type a custom value in the input field
+    // input value is retrieve and transformed to number type. same for its 'min' attribute
     const inputValue = parseFloat(this.inputfieldTarget.value)
     const inputMin = parseFloat(this.inputfieldTarget.min)
-    this.messageTarget.innerText = `Don minimum : ${this.inputfieldTarget.min}`
-
+    // initialize a text to display when relevant
+    this.messageTarget.innerText = `Don minimum : ${this.inputfieldTarget.min} €`
+    // check if field empty or typed value is less than min value
     if (this.inputfieldTarget.value.trim() === '' || inputValue < inputMin) {
+      // case either one is true, submit is not possible
+      // Therefore, message must be displayed whatever the execution flow
+
       console.log('nul ou inf')
       this.submitBtnTarget.setAttribute('disabled', 'true')
-      this.messageTarget.classList.toggle('hidden')
+      this.messageTarget.classList.remove('hidden')
 
     } else {
+      // case can only be greater
+      // submit is possible
+      // Therefore, message must be hidden whatever the execution flow
       console.log('sup')
       this.submitBtnTarget.removeAttribute('disabled')
       this.messageTarget.classList.add('hidden')
@@ -65,19 +83,38 @@ export default class extends Controller {
   }
 
   onBlur() {
-    console.log('blur');
+    // action blur (ie, when input field loose focus)
+    // get actuated each time user deselecte the input field
+    // this is to get back to starting point
+    // console.log('blur')
     this.showSubmit()
   }
 
   onInput() {
-    console.log('hit')
+    // event specific to the input field type
+    // get actuated each time the user type in
+    // console.log('hit')
     this.showSubmit()
   }
 
 
-  customValue() {
+  submitInput() {
+    // once submit possible, the user hit it to send custome value of the input field
+    // then, it must follow the usual flow (ie, same flow as quick buttons)
     console.log('donner hit');
     this.showConfirm()
+  }
+
+  cancelTip() {
+    // if user hit cancel on confirm page
+    // this is an endpoint of a user flow so many hidden and disabled stuff must be recalled here
+    // in order to get back to the starting point (complete circle)
+    confirmBox.classList.remove("-translate-y-full")
+    this.submitBtnTarget.setAttribute('disabled', 'true')
+    this.inputfieldTarget.value = ''
+    this.overlay.classList.add('hidden')
+    this.messageTarget.classList.add('hidden')
+
   }
 
   confirmTip() {
@@ -90,12 +127,6 @@ export default class extends Controller {
     this.checkout()
   }
 
-  cancelTip() {
-    // this.hideConfirmation()
-    confirmBox.classList.remove("-translate-y-full")
-    this.overlay.classList.add('hidden')
-
-  }
 
   checkout() {
     const donationValue = this.monDonTarget.getAttribute('value')
