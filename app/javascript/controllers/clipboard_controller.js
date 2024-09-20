7b167690739e047d0615376cf0e559aeef3e1c2a
Emre ALKAN <alkan.emre@hotmail.fr>
2024-09-20 12:32:04 +0200

    clipboard done

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -10,5 +10,23 @@ export default class extends Controller {
     event.preventDefault()
     // copy the source value in the clipboard
     navigator.clipboard.writeText(this.sourceTarget.value)
+    // inform copying succeeded
+    this.displayCopy(this.sourceTarget.value)
+  }
+
+  displayCopy(value){
+    this.sourceTarget.innerHTML = `${value} <i class="fa-solid fa-headset fa-xl"></i>`
+
+    const iconCopied = document.createElement('div')
+    iconCopied.classList.add('absolute', 'right-2', 'px-2', 'py-1', 'bg-gray-600', 'opacity-50', 'rounded-lg', 'text-white', 'text-sm')
+    iconCopied.innerHTML = ` Copié <i class="fa-solid fa-check" style="color: #11de0f;"></i>`
+
+    this.sourceTarget.classList.add('relative')
+    this.sourceTarget.append(iconCopied)
+
+    setTimeout(() => {
+      iconCopied.outerHTML = ``
+    }, 1000)
+
   }
 }
