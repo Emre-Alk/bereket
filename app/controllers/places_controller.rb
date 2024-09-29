257492fe6d4ead75abf0f6ebfa069fc35ebe9cc7
Emre ALKAN <alkan.emre@hotmail.fr>
2024-09-29 21:46:42 +0200

    debbug show page for nil score and review membership if not enrolled

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,11 +3,17 @@ class PlacesController < ApplicationController
   layout "profile"
 
   def show
+    # set the local to fr for this action only
     I18n.locale = :fr
     @place = Place.find(params[:id])
+    # identify donator if signed_in
     @donator = current_user&.donator
+    # find if place is in favorite list of donator
     @favorite = @place.favorites.where(donator: @donator).take
+    # retrieve all the review on this place
+    # 'includes' will load all donations and from all donators when loading place so that N+1 query is avoided
     @reviews = @place.reviews.includes(donation: :donator)
-    @score = @reviews.sum(&:rating).fdiv(@reviews.length)
+    # calculate the score based on ratings if there is at least one review else set to 0
+    @score = @reviews.presence ? @reviews.sum(&:rating).fdiv(@reviews.length) : 0
   end
 end
