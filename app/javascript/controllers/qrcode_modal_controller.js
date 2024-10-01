552699f77b6179e472e5e8e56c8dc27e09e0e89c
Emre ALKAN <alkan.emre@hotmail.fr>
2024-10-01 13:22:29 +0200

    some cleanup

--- app/javascript/controllers/qrcode_modal_controller.js
+++ app/javascript/controllers/qrcode_modal_controller.js
@@ -3,9 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="qrcode-modal"
 export default class extends Controller {
   static targets = ['modal']
-  connect() {
-    console.log('hello modal');
-  }
 
   toggle(){
     const modal = document.getElementById('modal')
