d7696f2e1155ca80a1a84f839b191e541418f244
Emre ALKAN <alkan.emre@hotmail.fr>
2024-10-19 22:50:08 +0200

    importmap stripe.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.3/dist/chart.js" # @4.4
 pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js" # "@kurkle--color.js" @0.3.2
 # pin "html5-qrcode", to: "https://unpkg.com/html5-qrcode@2.3.7/html5-qrcode.min.js" # @2.3.7
 pin "signature_pad", to: "https://cdn.jsdelivr.net/npm/signature_pad@5.0.2/+esm"
+pin "@stripe/stripe-js", to: "@stripe--stripe-js.js" # @4.8.0
