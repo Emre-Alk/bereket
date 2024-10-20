5d3d19094987b81fb330a0aac8e57d0f1e2116cd
Emre ALKAN <alkan.emre@hotmail.fr>
2024-10-20 17:30:22 +0200

    cleanup

--- app/controllers/assos/accounts_controller.rb
+++ app/controllers/assos/accounts_controller.rb
@@ -33,9 +33,6 @@ class Assos::AccountsController < AssosController
     service = StripeAccount.new(@account)
 
     account_token = set_account_params[:account_token]
-    puts '🟪🟪🟪🟪🟪🟪🟪'
-    puts "#{account_token}"
-    puts '🟪🟪🟪🟪🟪🟪🟪'
 
     service.create_account(account_token)
 
