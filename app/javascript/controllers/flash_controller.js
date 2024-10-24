33d1ec643808e91b7e77ec61786d12b4abe63063
Emre ALKAN <alkan.emre@hotmail.fr>
2024-10-24 16:28:27 +0200

    flash mvp1

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="flash"
+export default class extends Controller {
+  static targets = ['flash']
+
+  connect() {
+    setTimeout(() => {
+
+      this.flashTarget.classList.add('opacity-0') // Fades out the flash message
+      this.flashTarget.addEventListener('transitionend', () => {
+        this.flashTarget.remove() // Removes element after fade-out
+      })
+
+    }, 3000);
+  }
+}
