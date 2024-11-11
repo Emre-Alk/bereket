ee0ec2c0344fc7e5fb6cf5034bd551885fec1681
Emre ALKAN <alkan.emre@hotmail.fr>
2024-11-11 19:32:31 +0100

    debug

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,11 +24,12 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache); // Delete old caches
-          }
-        })
+        cacheNames.map((cache) => caches.delete(cache))
+        // cacheNames.map((cache) => {
+        //   if (cache !== CACHE_NAME) {
+        //     return caches.delete(cache); // Delete old caches
+        //   }
+        // })
       );
     })
   );
