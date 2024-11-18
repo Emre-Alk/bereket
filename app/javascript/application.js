4b0ab32ba860658c754c2fa45911ae7baf98da01
Emre ALKAN <alkan.emre@hotmail.fr>
2024-11-18 18:06:10 +0100

    removal of service worker code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-// register service-worker.js if navigator compatible
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
