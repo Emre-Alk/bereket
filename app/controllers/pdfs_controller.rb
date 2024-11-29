9070f755e5b6ce5588604d1cd1dd8f452f58c104
Emre ALKAN <alkan.emre@hotmail.fr>
2024-11-29 20:06:51 +0100

    essai

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class PdfsController < ApplicationController
   before_action :set_donator
   before_action :set_donation
@@ -112,9 +114,11 @@ class PdfsController < ApplicationController
 
   def download_pdf
     cerfa = @donator.cerfa
+    cerfa_path = Cloudinary::Utils.cloudinary_url(cerfa.url)
+    pdf_file = URI.parse(cerfa_path).open
 
     respond_to do |format|
-      format.pdf { send_data(cerfa.download, filename: cerfa.filename.to_s, type: cerfa.content_type.to_s, disposition: 'attachment')}
+      format.pdf { send_data(pdf_file, filename: cerfa.filename.to_s, type: cerfa.content_type.to_s, disposition: 'attachment')}
     end
   end
 
