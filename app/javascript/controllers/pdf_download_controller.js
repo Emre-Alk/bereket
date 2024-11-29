a5f07b6326d2f6609e01bbcdb5bdd9b3f4359ec5
Emre ALKAN <alkan.emre@hotmail.fr>
2024-11-29 20:55:17 +0100

    try new

---
+++ app/javascript/controllers/pdf_download_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="pdf-download"
+export default class extends Controller {
+  download() {
+    const url = this.data.get('url'); // PDF URL from data attribute
+    const filename = this.data.get('filename'); // Optional filename
+    this.downloadFile(url, filename);
+  }
+
+  downloadFile(url, filename) {
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        const a = document.createElement('a');
+        const objectUrl = URL.createObjectURL(blob);
+        a.href = objectUrl;
+        a.download = filename || 'file.pdf';
+        document.body.appendChild(a); // Required for some mobile browsers
+        a.click();
+        document.body.removeChild(a); // Clean up
+        URL.revokeObjectURL(objectUrl); // Release memory
+      })
+      .catch(error => {
+        console.error('Error downloading the file:', error);
+      });
+  }
+}
