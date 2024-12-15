e88cdceb531fb785d249698c569bd4df0616def6
Emre ALKAN <alkan.emre@hotmail.fr>
2024-12-15 21:46:18 +0100

    landing carousel feature cards done

--- app/javascript/controllers/carousel_sides_controller.js
+++ app/javascript/controllers/carousel_sides_controller.js
@@ -2,7 +2,8 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="carousel-sides"
 export default class extends Controller {
-  static targets = ["container", "items"];
+  static targets = ["container", "items"]
+  static classes = ["active", "inactive"]
 
   initialize(){
     this.active = 1
@@ -24,9 +25,9 @@ export default class extends Controller {
   endSwipe(event){
     const endX = event.changedTouches[0].clientX
     const diff = endX - this.startX
-    if (diff > 0) {
+    if (diff > 80) {
       this.prev()
-    } else {
+    } else if (diff < -80) {
       this.next()
     }
   }
@@ -48,14 +49,22 @@ export default class extends Controller {
     this.items[this.active].style.zIndex = 1
     this.items[this.active].style.filter = "none"
     this.items[this.active].style.opacity = 1
+    this.items[this.active].classList.remove(...this.inactiveClasses)
+    this.items[this.active].classList.add(...this.activeClasses)
+    this.items[this.active].dataset.active = 'active'
+
+
 
     for (let index = this.active + 1; index < this.items.length; index++) {
       stt++
       // this.items[index].style.transform = `translateX(${208*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`
       this.items[index].style.transform = `translateX(${208*stt}px) scale(${1 - 0.2*stt}) perspective(16px)`
       this.items[index].style.zIndex = -stt
+      this.items[index].classList.add(...this.inactiveClasses)
       // this.items[index].style.filter = "blur(5px)"
       this.items[index].style.opacity = stt > 2 ? 0 : 0.6
+      this.items[index].dataset.active = 'inactive'
+
     }
 
     stt = 0 // reset stepper
@@ -65,8 +74,10 @@ export default class extends Controller {
       // this.items[index].style.transform = `translateX(${-208*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`
       this.items[index].style.transform = `translateX(${-208*stt}px) scale(${1 - 0.2*stt}) perspective(16px)`
       this.items[index].style.zIndex = -stt
+      this.items[index].classList.add(...this.inactiveClasses)
       // this.items[index].style.filter = "blur(5px)"
       this.items[index].style.opacity = stt > 2 ? 0 : 0.6
+      this.items[index].dataset.active = 'inactive'
 
     }
   }
