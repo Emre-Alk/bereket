12743a5c4dbcff5f2f8c2a294daa0cfedb8a5aef
Emre ALKAN <alkan.emre@hotmail.fr>
2024-12-17 18:00:16 +0100

    bug on setinterval not working on pwa

--- app/javascript/controllers/carousel_sides_controller.js
+++ app/javascript/controllers/carousel_sides_controller.js
@@ -18,12 +18,14 @@ export default class extends Controller {
     this.load()
     this.containerTarget.addEventListener('touchstart', this.startSwipe.bind(this))
     this.containerTarget.addEventListener('touchend', this.endSwipe.bind(this))
-    this.resetTimeout()
+    document.addEventListener('DOMContentLoaded', this.resetTimeout.bind(this))
   }
 
   disconnect() {
     this.containerTarget.removeEventListener('touchstart', this.startSwipe())
     this.containerTarget.removeEventListener('touchend', this.endSwipe())
+    document.removeEventListener('DOMContentLoaded', this.resetTimeout.bind(this))
+
   }
 
   startSwipe(event){
