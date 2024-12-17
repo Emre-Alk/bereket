8c0e62cbb8f30e48d0f983e84e155384bf52b846
Emre ALKAN <alkan.emre@hotmail.fr>
2024-12-17 12:52:29 +0100

    indicator on carousel - each block of marketing as h-screen"

--- app/javascript/controllers/carousel_sides_controller.js
+++ app/javascript/controllers/carousel_sides_controller.js
@@ -2,13 +2,14 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="carousel-sides"
 export default class extends Controller {
-  static targets = ["container", "items"]
-  static classes = ["active", "inactive"]
+  static targets = ["container", "indicators"]
+  static classes = ["active", "inactive", "indicatorOn", "indicatorOff"]
 
   initialize(){
     this.active = 1
     this.items = this.containerTarget.children
     this.startX = 0
+    this.indicators = this.indicatorsTarget.children
   }
 
   connect(){
@@ -51,12 +52,14 @@ export default class extends Controller {
     let stt = 0 // stepper
 
     this.items[this.active].style.transform = 'none'
-    this.items[this.active].style.zIndex = 1
+    this.items[this.active].style.zIndex = this.items.length
     this.items[this.active].style.filter = "none"
     this.items[this.active].style.opacity = 1
     this.items[this.active].classList.remove(...this.inactiveClasses)
     this.items[this.active].classList.add(...this.activeClasses)
     this.items[this.active].dataset.active = 'active'
+    this.indicators[this.active].classList.remove(...this.indicatorOffClasses)
+    this.indicators[this.active].classList.add(...this.indicatorOnClasses)
 
 
 
@@ -64,9 +67,11 @@ export default class extends Controller {
       stt++
       // this.items[index].style.transform = `translateX(${208*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`
       this.items[index].style.transform = `translateX(${208*stt}px) scale(${1 - 0.2*stt}) perspective(16px)`
-      this.items[index].style.zIndex = -stt
+      this.items[index].style.zIndex = stt
       this.items[index].classList.remove(...this.activeClasses)
       this.items[index].classList.add(...this.inactiveClasses)
+      this.indicators[index].classList.remove(...this.indicatorOnClasses)
+      this.indicators[index].classList.add(...this.indicatorOffClasses)
       // this.items[index].style.filter = "blur(5px)"
       this.items[index].style.opacity = stt > 2 ? 0 : 0.6
       this.items[index].dataset.active = 'inactive'
@@ -79,9 +84,11 @@ export default class extends Controller {
       stt++
       // this.items[index].style.transform = `translateX(${-208*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`
       this.items[index].style.transform = `translateX(${-208*stt}px) scale(${1 - 0.2*stt}) perspective(16px)`
-      this.items[index].style.zIndex = -stt
+      this.items[index].style.zIndex = stt
       this.items[index].classList.remove(...this.activeClasses)
       this.items[index].classList.add(...this.inactiveClasses)
+      this.indicators[index].classList.remove(...this.indicatorOnClasses)
+      this.indicators[index].classList.add(...this.indicatorOffClasses)
       // this.items[index].style.filter = "blur(5px)"
       this.items[index].style.opacity = stt > 2 ? 0 : 0.6
       this.items[index].dataset.active = 'inactive'
