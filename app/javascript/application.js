7ed23141511d32b94d2f803b7ca80eea4d20071e
Emre ALKAN <alkan.emre@hotmail.fr>
2025-01-04 17:33:45 +0100

    delete sw

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => {
-      registration.unregister();
-      console.log("Service worker unregistered");
-    });
-  });
-}
