301e0826a0400d57e7cc8f21f9c29ac7ff9b10a4
Emre ALKAN <alkan.emre@hotmail.fr>
2025-02-05 12:46:16 +0100

    edit donator page done - remains: update user if changes require it / cerfa

--- app/javascript/controllers/file_controller.js
+++ app/javascript/controllers/file_controller.js
@@ -2,12 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="file"
 export default class extends Controller {
-  static targets = ['source', 'canvas']
+  static targets = ['source', 'canvas', 'canvasContainer', 'form', 'submitBtn']
 
   connect() {
-    console.log(this.sourceTarget);
-    console.log('canvas', this.canvasTarget)
+    this.formTarget.addEventListener('change', this.toggleSubmit.bind(this))
+    this.form = new FormData(this.formTarget)
+  }
 
+  disconnect() {
+    this.formTarget.removeEventListener('change', this.toggleSubmit.bind(this))
+  }
+
+  toggleSubmit(event) {
+    console.log('change detected', event.target)
+    console.log('form', this.form)
+
+    const selectInput = event.target.name
+    const newValue = event.target.value
+    console.log('select', selectInput);
+    console.log('newValue', newValue);
+
+    const oldValue = this.form.get(selectInput)
+    console.log('find', oldValue)
+
+    if (oldValue !== newValue) {
+      this.submitBtnTarget.removeAttribute('disabled')
+    }
   }
 
   openField() {
@@ -15,14 +35,11 @@ export default class extends Controller {
   }
 
   preview() {
-    console.log(this.sourceTarget)
-    console.log('canvas', this.canvasTarget)
-    // this.canvasTarget.classList.remove('hidden')
     const reader = new FileReader()
 
     reader.onload = function () {
+      this.canvasContainerTarget.classList.toggle('hidden')
       this.canvasTarget.src = reader.result
-      console.log('src', this.canvasTarget.src)
     }.bind(this)
 
     reader.readAsDataURL(this.sourceTarget.files[0])
