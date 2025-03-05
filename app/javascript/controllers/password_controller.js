187a0583b255d3c9fc2b5ea6f932df1bdc17e34e
Emre ALKAN <alkan.emre@hotmail.fr>
2025-03-05 15:39:27 +0100

    register form with confirm pw as hidden and JS handles to set it to pw value / pw with hide-unhide btn / register and session pages with link depending if has or not an account

---
+++ app/javascript/controllers/password_controller.js
@@ -0,0 +1,41 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="password"
+export default class extends Controller {
+
+  static targets = ['passwordInput', 'eyeIcon', 'passwordConfirmInput']
+
+  togglePassword(event) {
+    if (!event) {
+      this.passwordInputTarget.type = 'password'
+      this.eyeIconTarget.classList.remove('fa-eye')
+      this.eyeIconTarget.classList.add('fa-eye-slash')
+
+    } else {
+
+      if (this.passwordInputTarget.type === 'password') {
+        this.passwordInputTarget.type = 'text'
+        this.eyeIconTarget.classList.remove('fa-eye-slash')
+        this.eyeIconTarget.classList.add('fa-eye')
+      } else {
+        this.passwordInputTarget.type = 'password'
+        this.eyeIconTarget.classList.remove('fa-eye')
+        this.eyeIconTarget.classList.add('fa-eye-slash')
+      }
+
+    }
+  }
+
+  confirm(event) {
+    event.preventDefault()
+
+    // paste pw value to confirm field
+    this.passwordConfirmInputTarget.value = this.passwordInputTarget.value
+
+    // before submit, make sure that password field is type password
+    this.togglePassword()
+
+    // submit form
+    event.target.submit()
+  }
+}
